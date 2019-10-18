a5bd24cf530698e39669d55219165702ec15a347
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-10-18 13:04:45 +0200

    home URL fix -- REVERTED

--- src/components/App.js
+++ src/components/App.js
@@ -75,7 +75,7 @@ class RootApp extends Component {
                   exact
                   path={`${process.env.PUBLIC_URL || ""}/`}
                   render={() => (
-                    <Redirect to={`${process.env.PUBLIC_URL || ""}/front/home`} />
+                    <Redirect to={`${process.env.PUBLIC_URL || ""}/home`} />
                   )}
                 />
                 {this.routerContributions.map((route, index) => {

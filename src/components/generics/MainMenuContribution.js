5bdc338b87288195be458d618a3861433f6eb926
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2022-04-27 13:47:19 +0200

    Update MainMenuContribution.js

--- src/components/generics/MainMenuContribution.js
+++ src/components/generics/MainMenuContribution.js
@@ -118,10 +118,6 @@ class MainMenuContribution extends Component {
     this.redirect(route);
   };
   
-  menuPageUrl(route)  {
-    return this.props.modulesManager.getRef(route)
-  }
-
   redirect(route) {
     const { modulesManager, history } = this.props;
     _historyPush(modulesManager, history, route);
@@ -152,7 +148,7 @@ class MainMenuContribution extends Component {
                   <MenuList>
                     {this.props.entries.map((entry, idx) => (
                       <div key={`${this.props.header}_${idx}_menuItem`}>
-                        <MenuItem onClick={(e) => this.handleMenuSelect(e, entry.route)}  href = this.menuPageUrl(entry.route)>
+                        <MenuItem onClick={(e) => this.handleMenuSelect(e, entry.route)}  href={this.props.modulesManager.getRef(entry.route)}>
                           <ListItemIcon>{entry.icon}</ListItemIcon>
                           <ListItemText primary={entry.text}/>
                         </MenuItem>

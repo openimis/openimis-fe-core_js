061370821749ba647a326c885d6c0d495f1a961f
jdolkowski <jdolkowski@soldevelo.com>
2023-07-17 15:59:13 +0200

    CM-204: fix redux state

--- src/reducer.js
+++ src/reducer.js
@@ -95,10 +95,11 @@ function reducer(
       };
     case "CORE_CACHE_FILTER_RESET":
       const key = action.payload;
-      const { [key]: _, ...remainingFilters } = state.filtersCache;
+      const filtersCacheCopy = { ...state.filtersCache };
+      delete filtersCacheCopy[key];
       return {
         ...state,
-        filtersCache: remainingFilters,
+        filtersCache: filtersCacheCopy,
       };
 
     case "CORE_MUTATION_ADD":

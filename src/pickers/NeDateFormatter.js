c139a91b0f93057f1b2547b382d7fc2e696b9190
wzglinieckisoldevelo <98958634+wzglinieckisoldevelo@users.noreply.github.com>
2023-09-19 13:22:25 +0200

    ONI-155: Added formating option to Nepali calendar. (#156)
    * ONI-155: Added formating option to Nepali calendar.
    
    * ONI-155: Added second calendar display options config to readme.
    
    * ONI-155: Second calendar options code review fixes.
    
    * ONI-155: Second calendar default settings moved to constants.
    
    * ONI-155: Code review fixes for second calendar settings.

--- src/pickers/NeDateFormatter.js
+++ src/pickers/NeDateFormatter.js
@@ -1,14 +1,13 @@
-import NepaliDate from 'nepali-date-converter'
+import NepaliDate from "nepali-date-converter";
 
-export function formatDateFromISO(date) {
-  const secondCalendarFormat = ['DD MM YYYY', 'np']
+export function formatDateFromISO(date, formattingOptions) {
   if (!date) return "";
   // this nepali-date-converter only supports dates up to this date, it will crash whole site if this date is exceeded
   // added as a safeguard, other solution is probably needed
   // TODO remove after nepal component change
-  if (date > new Date("2090/12/30")){
+  if (date > new Date("2090/12/30")) {
     date = new Date("2090/12/30");
   }
-  let bsDate = new NepaliDate(new Date(date)).format(...secondCalendarFormat);
-  return bsDate
+  const bsDate = new NepaliDate(new Date(date)).format(...formattingOptions);
+  return bsDate;
 }

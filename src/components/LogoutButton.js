80fc0357605a75fa69150c60231d1ddf08e56a0b
Jan <j.dolkowski@soldevelo.com>
2023-11-07 10:38:46 +0100

    hotfix-saml-logout: clear local storage on logout

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { useDispatch } from "react-redux";
 import { IconButton } from "@material-ui/core";
 import { ExitToApp } from "@material-ui/icons";
 import { useHistory } from "../helpers/history";
@@ -15,6 +16,7 @@ const useStyles = makeStyles((theme) => ({
 
 const LogoutButton = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
   const modulesManager = useModulesManager();
   const mPassLogout = modulesManager.getConf("fe-core", "LogoutButton.showMPassProvider", false);
   const onClick = async (e) => {

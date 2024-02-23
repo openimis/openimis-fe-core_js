718668a1e771c4e130d01d31de95ebe5aca18c2a
olewandowski1 <olewandowski@soldevelo.com>
2024-02-23 16:14:05 +0100

    OP-1824: organise imports

--- src/components/generics/WarningBox.js
+++ src/components/generics/WarningBox.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Grid, Box, Typography } from "@material-ui/core";
 import PropTypes from "prop-types";
+
+import { Grid, Box, Typography } from "@material-ui/core";
 
 const DEFAULT_STYLES = {
   backgroundColor: "khaki",

4dab757d68ad708e2f67c916fc98609fd98544d4
Oliver Lewandowski <109145288+olewandowski1@users.noreply.github.com>
2024-03-01 10:35:18 +0100

    OP-1830: enable chaning min value of amount input - default 0 (#203)

--- src/components/inputs/AmountInput.js
+++ src/components/inputs/AmountInput.js
@@ -1,20 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import { injectIntl } from "react-intl";
 import { InputAdornment } from "@material-ui/core";
 import NumberInput from "./NumberInput";
 import { useModulesManager } from "../../helpers/modules";
 
-const AmountInput = ({ intl, ...props }) => {
+const AmountInput = ({ intl, inputMinValue = 0, ...props }) => {
   const modulesManager = useModulesManager();
   const position = modulesManager.getConf("fe-core", "AmountInput.currencyPosition", "start");
+
   if (!["start", "end"].includes(position)) {
     throw new Error(`Position ${position} is not accepted. Only 'start' and 'end' are valid options.`);
   }
+
   const extraProps = {
     [`${position}Adornment`]: (
       <InputAdornment position={position}>{intl.formatMessage({ id: "currency" })}</InputAdornment>
     ),
+    min: inputMinValue,
   };
+
   return <NumberInput {...props} {...extraProps} />;
 };
 

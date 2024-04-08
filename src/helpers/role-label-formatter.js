d82deb4f6348a43738fa93c4fbef298fa3253a92
olewandowski1 <olewandowski@soldevelo.com>
2024-04-08 13:53:20 +0200

    OP-1825: sonar fix

--- src/helpers/role-label-formatter.js
+++ src/helpers/role-label-formatter.js
@@ -3,7 +3,7 @@ const RIGHT_NAME_OMITTED_WORDS = ["gql", "mutation", "perms"];
 const QUERY_STRING = "query";
 const SEARCH_STRING = "search";
 const WHITESPACE = " ";
-const WORD_REGEX = /^\w|\s\w/g;
+const WORD_REGEX = /^(\w|\s\w)/g;
 
 const capitalizeFirstLetter = (string) => {
   return string.toLowerCase().replace(WORD_REGEX, (letter) => letter.toUpperCase());

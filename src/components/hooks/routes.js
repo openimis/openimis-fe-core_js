56693d0a804541d6ca11ed2b7803813a3ae64cf3
sancty <sanctifieryawmensah55@gmail.com>
2025-11-17 16:00:43 +0100

    fix: update title in routes for medical services

--- src/components/hooks/routes.js
+++ src/components/hooks/routes.js
@@ -133,7 +133,7 @@ const fosa = [
   },
   {
     parent: "Fosa",
-    title: "Panier des soins : actes des soins",
+    title: "Paquet des soins : actes des soins",
     path: "/medical/pricelists/services",
   },
   {

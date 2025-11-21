f9c9abb47c2c852f1b5b3aae0273a16741c118eb
sancty <sanctifieryawmensah55@gmail.com>
2025-11-21 13:28:05 +0100

    fix: update titles in routes for medical services to correct French translations

--- src/components/hooks/routes.js
+++ src/components/hooks/routes.js
@@ -133,12 +133,12 @@ const fosa = [
   },
   {
     parent: "Fosa",
-    title: "Paquet des soins : actes des soins",
+    title: "Paquet de soins : actes des soins",
     path: "/medical/pricelists/services",
   },
   {
     parent: "Fosa",
-    title: "Paquet des soins: produits medicaux",
+    title: "Paquet de soins: produits medicaux",
     path: "/medical/pricelists/items",
   },
   {
